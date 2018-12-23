83115a59d3b1a5d85f94251beb7a9251b02213fd
Ali Tabesh <ali.tabesh76@gmail.com>
2018-12-23 11:56:46 +0330

    final

---
+++ index.php
@@ -0,0 +1,489 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<!-- Mirrored from themes.89elements.com/definity-html/index-animated-mp.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 08 Apr 2018 12:50:51 GMT -->
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>شرکت بین المللی سهدیس</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Favicon -->
+
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="assets/styles/vendor/bootstrap.min.css">
+        <!-- Fonts -->
+        <link rel="stylesheet" href="assets/fonts/et-lineicons/css/style.css">
+        <link rel="stylesheet" href="assets/fonts/linea-font/css/linea-font.css">
+        <link rel="stylesheet" href="assets/fonts/fontawesome/css/font-awesome.min.css">
+        <!-- Slider -->
+        <link rel="stylesheet" href="assets/styles/vendor/slick.css">
+        <!-- Lightbox -->
+        <link rel="stylesheet" href="assets/styles/vendor/magnific-popup.css">
+        <!-- Animate.css -->
+        <link rel="stylesheet" href="assets/styles/vendor/animate.css">
+        <!-- Animated Headings -->
+        <link rel="stylesheet" href="assets/styles/vendor/animated-heading.css">
+
+        <link rel="stylesheet" href="assets/fonts/fonts/font.css">
+
+        <!-- Definity CSS -->
+        <link rel="stylesheet" href="assets/styles/main.css">
+        <link rel="stylesheet" href="assets/styles/responsive.css">
+
+        <!-- JS -->
+        <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body id="page-top">
+        <!-- Google Tag Manager -->
+        <noscript><iframe src="http://www.googletagmanager.com/ns.html?id=GTM-MH7TSF"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '../../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MH7TSF');</script>
+        <!-- End Google Tag Manager -->
+
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+
+
+        <!-- ========== Preloader ========== -->
+
+        <div class="preloader">
+          <img src="assets/images/loader.svg" alt="Loading...">
+        </div>
+
+
+
+        <!-- ========== Navigation ========== -->
+
+        <nav class="navbar navbar-default navbar-fixed-top mega navbar-trans">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+
+
+            </div><!-- / .navbar-header -->
+
+            <!-- Navbar Links -->
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+
+
+              </ul><!-- / .nav .navbar-nav -->
+
+
+              <!-- Navbar Links Right -->
+              <ul class="nav navbar-nav navbar-right">
+
+                <!-- Elements -->
+                <li class="dropdown">
+                  <a href="#contact" class="dropdown-toggle" data-delay="350" role="button" aria-haspopup="true" aria-expanded="false">تماس با ما</a>
+                </li><!-- / Elements -->
+
+
+                <!-- Pages -->
+                <li class="dropdown mega-fw">
+                  <a href="about.html" class="dropdown-toggle" data-delay="350" role="button" aria-haspopup="true" aria-expanded="false">درباره ما</a>
+                </li><!-- / Pages -->
+
+                <!-- Blog -->
+                <li class="dropdown">
+                  <a href="sales.html" class="dropdown-toggle" data-delay="350" role="button" aria-haspopup="true" aria-expanded="false">محصولات</a>
+                </li><!-- / Blog -->
+
+
+                <!-- Shop -->
+                <li class="dropdown">
+                  <a href="http://sahdis.com/#home" class="dropdown-toggle" data-delay="350" role="button" aria-haspopup="true" aria-expanded="false">صفحه اصلی</a>
+                </li><!-- / Blog -->
+              </ul><!-- / .nav .navbar-nav .navbar-right -->
+
+            </div><!--/.navbar-collapse -->
+          </div><!-- / .container -->
+        </nav><!-- / .navbar -->
+
+
+
+        <!-- ========== Hero Cover ========== -->
+
+        <div id="home" class="main-demo-hero">
+          <div class="bg-overlay">
+
+            <!-- Hero Content -->
+            <div class="hero-content-wrapper">
+              <div class="hero-content">
+
+                <h1 class="hero-lead wow fadeInRight" style="font-family:'Open Sans'!important ; font-size:50px ; color:rgb(211, 199, 199)" data-wow-duration="1.5s">شرکت بین المللی سهدیس</h1>
+                <h4 class="h-alt hero-subheading wow fadeIn" style="font-size:20px" data-wow-duration="2s" data-wow-delay=".7s">در ایران Acceptto نمایندگی رسمی</h4>
+
+              </div>
+            </div>
+
+            <!-- Scroller -->
+            <a href="#about" class="scroller scroller-dark">
+              <span class="linea-basic-magic-mouse"></span>
+            </a>
+
+          </div><!-- / .bg-overlay -->
+        </div><!-- / .main-hero-2 -->
+
+
+
+        <!-- ========== About - Section ========== -->
+
+        <section id="about" class="container">
+          <div class="row section">
+
+            <header class="sec-heading rtl-text">
+              <h2>سهدیس را بشناسید.</h2>
+            </header>
+
+            <div class="col-md-offset-1 col-md-5 rtl-text">
+              <p>سهدیس همچنین ارائه دهنده خدماتی چون تولید محصولات نرم افزاری و فروش انواع کامپیوترهای رومیزی و لپ تاپ و  لوازم جانبی کامپیوتر نیز هست.
+              </p>
+            </div>
+            <div class="col-md-5 text-right rtl-text">
+              <p>سهدیس نماینده رسمی شرکت Acceptto در ایران است.Acceptto یک شرکت فعال در زمینه امنیت و احراز هویت است که شیوه جدیدی برای احراز هویت اشخاص در زمینه دیجیتال فراهم کرده است.</p>
+            </div>
+          </div><!-- / .row -->
+          <div class="row ws-m">
+            <div class="col-md-12 text-center">
+              <a href="about.html" class="btn-text">بیشتر درباره ما</a>
+            </div>
+          </div>
+        </section><!-- / .container -->
+
+
+
+        <!-- ========== Number Counters - Parallax ========== -->
+
+        <div class="number-counters">
+          <div class="container">
+            <div id="counters" class="row count-wrapper">
+
+              <div class="col-md-3 mb-sm-100 count-item wow fadeIn" style="float:none;margin:0 auto" data-wow-duration="5s" data-wow-delay="0.1s">
+                <span id="count-1" class="count-nbr">18</span>
+                <span class="count-text">سال سابقه</span>
+              </div>
+
+            </div><!-- / .row -->
+          </div><!-- / .container -->
+        </div><!-- / .number-counters -->
+
+
+
+        <!-- ========== Services ========== -->
+
+        <section class="container ft-centered" style="padding-top:160px">
+
+          <header class="sec-heading">
+            <h2>خدمات ما</h2>
+          </header>
+
+          <div class="row" style="padding-bottom:70px">
+
+            <!-- Item 1 -->
+            <div class="col-md-3 col-sm-6 mb-sm-50 ft-item ft-material wow zoomIn" data-wow-duration=".6s" data-wow-delay=".3s">
+              <span class="et-tools"></span>
+              <h5>طراحی و مدیریت شبکه های کامپیوتری</h5>
+            </div>
+
+            <!-- Item 2 -->
+            <div class="col-md-3 col-sm-6 mb-sm-50 ft-item ft-material wow zoomIn" data-wow-duration=".6s">
+              <span class="et-desktop"></span>
+              <h5>فروش نرم افزارهای کامپیوتری</h5>
+            </div>
+
+            <!-- Item 3 -->
+            <div class="col-md-3 col-sm-6 mb-sm-50 ft-item ft-material wow zoomIn" data-wow-duration=".6s">
+              <span class="et-anchor"></span>
+              <h5>مشاوره در زمینه امنیت شبکه های کامپیوتری</h5>
+            </div>
+
+            <!-- Item 4 -->
+            <div class="col-md-3 col-sm-6 mb-sm-50 ft-item ft-material wow zoomIn" data-wow-duration=".6s" data-wow-delay=".3s">
+              <span class="et-strategy"></span>
+              <h5>فروش رایانه شخصی و لوازم جانبی</h5>
+            </div>
+
+          </div><!-- / .row -->
+          <div class="row ws-m">
+            <div class="col-md-12 text-center">
+              <a href="sales.html" class="btn-text">سایر خدمات</a>
+            </div>
+          </div>
+        </section><!-- / .ft-frames -->
+
+
+        <!-- ========== Featured Projects ========== -->
+
+        <div class="container-fluid ft-hover-item">
+          <div class="row">
+
+            <!-- Item 2 -->
+            <div class="ft-item ft-2">
+              <div class="bg-overlay">
+
+                <div class="content-wrapper">
+                  <a href="pages/portfolio/portfolio-single-1.html"><h3>Acceptto بیشتر درباره</h3></a>
+                  <p>ارائه راه حل های امینتی سازمانی<br> از طریق اعتبار سنجی هوشمند</p>
+                  <a href="pages/portfolio/portfolio-single-1.html" class="btn-ghost btn-ghost-light ft-button">بیشتر</a>
+                </div>
+
+              </div><!-- / .bg-overlay -->
+            </div><!-- / .ft-item -->
+
+          </div><!-- / .row -->
+        </div><!-- / .container-fluid -->
+
+
+
+
+
+
+        <!-- ========== Footer Contact ========== -->
+
+        <footer id="contact" class="footer-contact">
+          <div class="container-fluid">
+            <div class="row">
+
+              <!-- Map and address -->
+              <div class="col-lg-6 no-gap contact-info">
+
+                <!-- Show Info Button -->
+                <a href="#" class="show-info-link"><i class="fa fa-info"></i>بازگشت</a>
+
+                <div id="map-canvas" class="footer-map"></div>
+
+                <address class="contact-info-wrapper">
+                  <ul>
+                    <!-- Address -->
+                    <li class="contact-group">
+                      <span class="adr-heading">دفتر مرکزی</span>
+                      <span class="adr-info">گرگان٬ولیعصر٬جنب بانک تجارت</span>
+                    </li>
+                    <!-- Email -->
+                    <li class="contact-group">
+                      <span class="adr-heading">دفتر فنی</span>
+                      <span class="adr-info">گرگان٬ولیعصر٬عدالت سی و چهار٬مجتمع نوین</span>
+                    </li>
+                  </ul>
+                  <ul>
+                    <!-- Phone -->
+                    <li class="contact-group">
+                      <span class="adr-heading">تلفن پشتیبانی</span>
+                      <span class="adr-info">۰۱۷۳۲۲۵۰۳۷۰ | ۰۱۷۳۲۲۲۹۵۲۰ | ۰۱۷۳۲۳۴۲۶۴۷</span>
+                    </li>
+                    <!-- Mobile -->
+                    <li class="contact-group">
+                      <span class="adr-heading">ایمیل</span>
+                      <span class="adr-info">info@sahdis.com</span>
+                    </li>
+                  </ul>
+
+                  <a href="#" class="show-map"><span class="linea-basic-geolocalize-05"></span>نمایش روی نقشه</a>
+                </address>
+
+              </div><!-- / .col-lg-6 -->
+
+
+              <!-- Contact Form -->
+              <div class="col-lg-6 no-gap section contact-form">
+                <header class="sec-heading">
+                  <h2>چگونه میتوانیم به شما کمک کنیم؟</h2>
+                </header>
+
+                <form action="/" method="POST" class="form-ajax wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s">
+
+                  <!-- Name -->
+                  <div class="form-group">
+                    <input type="text" name="name" id="name-contact-1" class="form-control validate-locally" placeholder="نام خود را وارد کنید">
+                    <label for="name-contact-1"></label>
+                    <span class="pull-right alert-error"></span>
+                  </div>
+
+                  <!-- Email -->
+                  <div class="form-group">
+                    <input type="email" name="email" id="email-contact-1" class="form-control validate-locally" placeholder="ایمیل خود را وارد کنید">
+                    <label for="email-contact-1"></label>
+                    <span class="pull-right alert-error"></span>
+                  </div>
+
+                  <!-- Message -->
+                  <div class="form-group">
+                    <textarea class="form-control" name="message" id="message-contact-1" rows="5" placeholder="متن پیام"></textarea>
+                    <label for="message-contact-1"></label>
+                  </div>
+                  <input type="submit" class="btn pull-right" value="ارسال">
+
+                  <!-- Ajax Message -->
+                  <div class="ajax-message col-md-12 no-gap"></div>
+
+                </form>
+              </div><!-- / .col-lg-6 -->
+
+            </div><!-- / .row -->
+          </div><!-- / .container-fluid -->
+
+
+          <!-- Social Links -->
+          <div class="dark-bg">
+            <div class="container footer-social-links">
+              <div class="row">
+
+                <ul>
+                  <li><a href="#">facebook</a></li>
+                  <li><a href="#">Twitter</a></li>
+                  <li><a href="#">Dribbble</a></li>
+                  <li><a href="#">Behance</a></li>
+                </ul>
+
+              </div>
+            </div><!-- / .container -->
+          </div><!-- / .dark-bg -->
+
+
+          <!-- Copyright -->
+          <div class="copyright">
+            <div class="container">
+              <div class="row">
+
+                <div class="col-md-6">
+                  <small>&copy; 2018 Sahdis. Made by <a class="no-style-link" href="https://www.linkedin.com/in/majid-shamoushaki-3250a1110" target="_blank">Jidmaa</a></small>
+                </div>
+
+                <div class="col-md-6">
+                  <small><a href="#page-top" class="pull-right to-the-top">ابتدای صفحه<i class="fa fa-angle-up"></i></a></small>
+                </div>
+
+              </div><!-- / .row -->
+            </div><!-- / .container -->
+          </div><!-- / .copyright -->
+        </footer><!-- / .footer-contact -->
+
+
+
+        <!-- ========== Scripts ========== -->
+
+        <script src="assets/js/vendor/jquery-2.1.4.min.js"></script>
+        <script src="assets/js/vendor/google-fonts.js"></script>
+        <script src="assets/js/vendor/jquery.easing.js"></script>
+        <script src="assets/js/vendor/jquery.waypoints.min.js"></script>
+        <script src="assets/js/vendor/bootstrap.min.js"></script>
+        <script src="assets/js/vendor/bootstrap-hover-dropdown.min.js"></script>
+        <script src="assets/js/vendor/smoothscroll.js"></script>
+        <script src="assets/js/vendor/jquery.localScroll.min.js"></script>
+        <script src="assets/js/vendor/jquery.scrollTo.min.js"></script>
+        <script src="assets/js/vendor/jquery.stellar.min.js"></script>
+        <script src="assets/js/vendor/jquery.parallax.js"></script>
+        <script src="assets/js/vendor/slick.min.js"></script>
+        <script src="assets/js/vendor/jquery.easypiechart.min.js"></script>
+        <script src="assets/js/vendor/countup.min.js"></script>
+        <script src="assets/js/vendor/isotope.min.js"></script>
+        <script src="assets/js/vendor/jquery.magnific-popup.min.js"></script>
+        <script src="assets/js/vendor/wow.min.js"></script>
+        <script src="assets/js/vendor/animated-heading.js"></script>
+        <script src="assets/js/vendor/jquery.ajaxchimp.js"></script>
+
+        <!-- Google Maps -->
+        <script src="assets/js/gmap.js"></script>
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCYColqjl3JW6XXTbptNuBg5i8Z8P6YkUM&callback=myMap"></script>
+
+        <!-- Definity JS -->
+        <script src="assets/js/main.js"></script>
+    </body>
+
+<!-- Mirrored from themes.89elements.com/definity-html/index-animated-mp.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 08 Apr 2018 12:50:52 GMT -->
+</html>
+
+<?php
+if(isset($_POST['email'])) {
+
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "info@sahdis.com";
+    $email_subject = "Your email subject line";
+
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+
+
+    // validation expected data exists
+    if(!isset($_POST['name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['message'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');
+    }
+
+
+
+    $name = $_POST['name']; // required
+    $email_from = $_POST['email']; // required
+    $message = $_POST['message']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'ایمیل وارد شده معتبر نمیباشد<br />';
+  }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+  if(!preg_match($string_exp,$name)) {
+    $error_message .= 'نام وارد شده معتبر نمیباشد<br />';
+  }
+
+  if(strlen($message) < 2) {
+    $error_message .= 'پیام وارد شده معتبر نمیباشد<br />';
+  }
+
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+
+    $email_message = "Form details below.\n\n";
+
+
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+
+
+
+    $email_message .= "Name: ".clean_string($name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Message: ".clean_string($message)."\n";
+
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+<!-- include your own success html here -->
+
+با تشکر از پیام شما.به زودی با شما تماس خواهیم گرفت.
+
+<?php
+
+}
+?>
